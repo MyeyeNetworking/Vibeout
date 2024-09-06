10edf85a9950e02c392c770b9a8cf2039e728314
Myeye_876 <paulbinger11@gmail.com>
2024-09-05 20:32:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -31,10 +31,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-// Hamburger menu toggle
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
 
-hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
