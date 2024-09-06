ed905a291409622a7bde6bf7e47f69f1598d2972
Myeye_876 <paulbinger11@gmail.com>
2024-09-05 20:49:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,13 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(showNextSlide, 3000); // Change slide every 3 seconds
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navMenu = document.getElementById('nav-menu');
 
-    menuToggle.addEventListener('click', function() {
-        navMenu.classList.toggle('active');
-    });
 
 
 
