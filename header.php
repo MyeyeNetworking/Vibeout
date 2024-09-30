0d601798bac46467d60f5fe53acb71ca71845071
Myeye_876 <paulbinger11@gmail.com>
2024-09-29 21:00:42 -0500

    A

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VibeOut</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+
+  <audio id="background-audio" autoplay controlS loop></audio>
+        <source src="image/BG.music.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+<header>
+    <div class="logo">VIBEO</div>
+    <button class="menu-toggle">☰</button> <!-- Mobile menu toggle button -->
+    <nav>
+        <ul class="nav-menu">
+            <li><a href="#" data-page="home">Home</a></li>
+            <li><a href="#" data-page="genra">Genra</a></li>
+            <li><a href="#" data-page="promote">Promote Event</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
